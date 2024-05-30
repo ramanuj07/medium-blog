e276594560538c668a477bb4dc8669e2a8f632ba
Ramanuj <ramanuj0705@gmail.com>
2024-05-30 09:39:56 +0530

    routing in FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Blog from "./pages/Blog";
+
 function App() {
   return (
-    <>
-      <h1 className="font-bold text-3xl text-center">Medium Website</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
